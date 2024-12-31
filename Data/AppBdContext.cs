5171362868bf6acc7b03fc75b23d632e4a187788
rowanabisaiutp <a.rowan.ojeda@utponiente.edu.mx>
2024-12-31 13:56:30 -0600

    nueva API rest

---
+++ Data/AppBdContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace api_restful_net__.Models;
+
+namespace api_restful_net__.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using api_restful_net__.Models.api_restful_net__.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuración del DbContext para usar MySQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
